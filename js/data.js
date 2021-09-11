c3480000590613b6ee2be294f13532cf80738c4d
prkm93 <aradshub93@gmail.com>
2021-09-11 08:36:19 +0530

    added quiz questions

---
+++ js/data.js
@@ -0,0 +1,42 @@
+export const quiz = [
+    {
+        q1:"What is the third angle for the triangle where angle1 = 45° and angle2 = 45°?",
+        a1: "90°"
+    },
+    {
+        q2:"Which one will be the type of triangle if angle1 = 45° and angle2 = 45°?",
+        a2: "right angled"
+    },
+    {
+        q3:"A triangle can have",
+        a3:"one right angle"
+    },
+    {
+        q4:"Which of the following can form a right angled triangle?",
+        a4:"12, 16, 20"
+    },
+    {
+        q5:"Which of the following triangles are always similar?",
+        a5:"Equilateral triangle"
+    },
+    {
+        q6:"If one angle of a triangle is obtuse, then which one of the following is the possible measure of remaining angles?",
+        a6:"85°"
+    },
+    {
+        q7:"If the largest angle in a triangle is 70°, what is the least possible value of the smallest angle of the triangle?",
+        a7:"10°"
+    },
+    {
+        q8:"The perimeter of scalene triangle with sides a, b, c is",
+        a8:"a + b + c"
+    },
+    {
+        q9:"A scalene triangle has ___ lines of symmetry",
+        a9:"no"
+    },
+    {
+        q10:"There is a right triangle PQR where: angle Q = 90°; angle P = angle R. What is the measure of angles P and R?",
+        a10:"45°"
+    }
+]
