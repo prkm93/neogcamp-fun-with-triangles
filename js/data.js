ac06a688985877b974c568786ac2b230705e1458
prkm93 <aradshub93@gmail.com>
2021-09-12 11:09:02 +0530

    fixed rendering logic

--- js/data.js
+++ js/data.js
@@ -1,52 +1,52 @@
 export const quiz = [
     {
-        q1:"What is the third angle for the triangle where angle1 = 45° and angle2 = 45°?",
-        o1:["45°","90°","60°"],
-        a1: "90°"
+        q:"What is the third angle for the triangle where angle1 = 45° and angle2 = 45°?",
+        o:["45°","90°","60°"],
+        a: "90°"
     },
     {
-        q2:"Which one will be the type of triangle if angle1 = 45° and angle2 = 45°?",
-        o2:["obtuse","acute","right angled"],
-        a2: "right angled"
+        q:"Which one will be the type of triangle if angle1 = 45° and angle2 = 45°?",
+        o:["obtuse","acute","right-angled"],
+        a: "right-angled"
     },
     {
-        q3:"A triangle can have",
-        o3:["one right angle", "two right angles"],
-        a3:"one right angle"
+        q:"A triangle can have",
+        o:["one-right-angle", "two-right-angles", "three-right-angles"],
+        a:"one-right-angle"
     },
     {
-        q4:"Which of the following can form a right angled triangle?",
-        o4:["14, 15, 26", "12, 16, 20"],
-        a4:"12, 16, 20"
+        q:"Which of the following can form a right angled triangle?",
+        o:["14, 15, 26", "12, 16, 20", "10, 20, 30"],
+        a:"12,16,20"
     },
     {
-        q5:"Which of the following triangles are always similar?",
-        o5:["Equilateral triangle", "Isosceles triangle"],
-        a5:"Equilateral triangle"
+        q:"Which of the following triangles are always similar?",
+        o:["Equilateral-triangle", "Isosceles-triangle", "Scalene-Triangle"],
+        a:"Equilateral-triangle"
     },
     {
-        q6:"If one angle of a triangle is obtuse, then which one of the following is the possible measure of remaining angles?",
-        o6:["100°", "85°"],
-        a6:"85°"
+        q:"If one angle of a triangle is obtuse, then which one of the following is the possible measure of remaining angles?",
+        o:["100°", "85°", "120"],
+        a:"85°"
     },
     {
-        q7:"If the largest angle in a triangle is 70°, what is the least possible value of the smallest angle of the triangle?",
-        o7:["30°", "10°"],
-        a7:"10°"
+        q:"If the largest angle in a triangle is 70°, what is the least possible value of the smallest angle of the triangle?",
+        o:["30°", "10°", "80°"],
+        a:"10°"
     },
     {
-        q8:"The perimeter of scalene triangle with sides a, b, c is",
-        o8:["a + b + c", "2a", "None of these"], 
-        a8:"a + b + c"
+        q:"The perimeter of scalene triangle with sides a, b, c is",
+        o:["a+b+c", "2a", "None-of-these"], 
+        a:"a+b+c"
     },
     {
-        q9:"A scalene triangle has ___ lines of symmetry",
-        o9:["two","no","15"],
-        a9:"no"
+        q:"A scalene triangle has ___ lines of symmetry",
+        o:["two","no","15"],
+        a:"no"
     },
     {
-        q10:"There is a right triangle PQR where: angle Q = 90°; angle P = angle R. What is the measure of angles P and R?",
-        o10:["85°","65°","45°"],
-        a10:"45°"
+        q:"There is a right triangle PQR where: angle Q = 90°; angle P = angle R. What is the measure of angles P and R?",
+        o:["85°","65°","45°"],
+        a:"45°"
     }
 ]
--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
+import {quiz} from './data.js';
 const quizSection = document.querySelector('#questions');
+const submitBtn = document.querySelector('#btn-submit');
+
+console.log(quiz);
+
+const renderQuestions = () => {
+
+    quiz.forEach((item, i) => {
+
+        const uniqueItem0 = `${item.o[0].split(" ").join("")}`;
+        const uniqueItem1 = `${item.o[1].split(" ").join("")}`;
+        const uniqueItem2 = `${item.o[2].split(" ").join("")}`;
+        const questionTemplate = `<div class="each-question">
+                                    <p>${item.q}</p>
+                                    <input type="radio" name=${i}  value=${uniqueItem0}>
+                                    <label for=${uniqueItem0}>${uniqueItem0}</label>
+                                    <input type="radio" name=${i}  value=${uniqueItem1}>
+                                    <label for=${uniqueItem1}>${uniqueItem1}</label>
+                                    <input type="radio" name=${i}  value=${uniqueItem2}>
+                                    <label for=${uniqueItem2}>${uniqueItem2}</label>
+                                  </div>`;  
+        quizSection.innerHTML += questionTemplate;
+    })
+    
+}
+
+
+console.log(quizSection);
+
+document.addEventListener('DOMContentLoaded', renderQuestions);
